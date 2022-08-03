2b88ff7d4ab7f1f216566a175bc25a8c4b39ab74
hongda <1993xiehongda@gmail.com>
2022-08-03 21:00:58 +1000

    Basket API and Catalog API

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext: ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var settings =
+                MongoClientSettings.FromConnectionString(
+                    configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            settings.SslSettings = new SslSettings()
+            {
+                EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12
+            };
+            var client = new MongoClient(settings);
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
